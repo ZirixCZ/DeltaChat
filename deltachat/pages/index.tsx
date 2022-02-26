80064c1e29d91f123b74b405f6ff500e639ab190
Michael <zirixcz@gmail.com>
2022-02-26 15:46:59 +0100

    TypeScript upgrade
    added typescript

---
+++ deltachat/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Template from '../components/Template/Template'
+
+export default function Home() {
+  return (
+    <>
+      <Template />
+      <Template />
+    </>
+  )
+}
