7d42becbc4e74ec7241c23f4dc99d1f79e5e1535
Michael <zirixcz@gmail.com>
2022-03-01 18:28:49 +0100

    Improving the template
    This is a template for us to follow

--- src/components/Template/Template.tsx
+++ src/components/Template/Template.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './style/template.module.css'
 
-const Template = () => {
+type TemplateProps = {
+    text: string;
+    children?: JSX.Element | JSX.Element[];
+}
+
+const Template = ({ text, children }: TemplateProps ) => {
+    // [watched variable, update function] = useState(innitial value)
+    const [count, setCount] = useState(0)
+    const handleButtonClick = (e) => {
+        e.preventDefault();
+        // update state variable count
+        setCount(count + 1)
+    }
     return (
-        <h1>Hello</h1>
+        // has to have one parent element. <></> can help.
+
+            <div className={`${styles.centereddiv} ${styles.colorblue}`}>
+                {children}
+                <h1 id={`${styles.fuckoffnext}`}>{text}</h1>
+                <button onClick={handleButtonClick}>{count}</button>
+            </div>
     )
 };
 export default Template
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import Template from '../components/Template/Template'
 export default function Home() {
   return (
     <>
-      <Template />
-      <Template />
+      {/* You can use the component more times */}
+      <Template text="Sup" />
+      <Template text="How are you" ><p>I am a children</p></Template>
     </>
   )
 }
