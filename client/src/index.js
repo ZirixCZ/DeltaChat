e21a618af8635e038a1dea0dcbd207f4b344fa71
Michael <zirixcz@gmail.com>
2022-03-16 16:04:40 +0100

    Fixing #a787685

--- client/src/index.js
+++ client/src/index.js
@@ -24,45 +24,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// // Import the functions you need from the SDKs you need
 
-// import { initializeApp } from "firebase/app";
-
-// import { getAnalytics } from "firebase/analytics";
-
-// // TODO: Add SDKs for Firebase products that you want to use
-
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// // Your web app's Firebase configuration
-
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// const firebaseConfig = {
-
-//   apiKey: "AIzaSyCQy85SFHGOxVHlunkQJEBi6iGEWiuHbeg",
-
-//   authDomain: "deltachat-62183.firebaseapp.com",
-
-//   projectId: "deltachat-62183",
-
-//   storageBucket: "deltachat-62183.appspot.com",
-
-//   messagingSenderId: "937050101854",
-
-//   appId: "1:937050101854:web:4a8fe4ddabfc5f419a7ccf",
-
-//   measurementId: "G-W0YCJXG7HR"
-
-// };
-
-
-// // Initialize Firebase
-
-// const app = initializeApp(firebaseConfig);
-
-// const analytics = getAnalytics(app);
 console.log("HELLO")
 console.log(process.env.APIKEY)
 ReactDOM.render(
