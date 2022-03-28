7785e6b8b2bf9ba4842e50da3134fe34923c8326
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2022-03-28 21:07:13 +0200

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,78 +1,88 @@
-import React, { useState, useEffect, useRef } from 'react';
-import socketClient from 'socket.io-client';
-import './index.css';
+import React, { useState, useEffect, useRef } from "react";
+import socketClient from "socket.io-client";
+import "./index.css";
 
 export default function App() {
   const SERVER = "https://detla-chat-server.herokuapp.com/";
-  const [status, setStatus] = useState('not connected');
+  const [status, setStatus] = useState("not connected");
   const [message, setMessage] = useState(null);
-  const [socket] = useState(() => { return socketClient(SERVER) });
+  const [socket] = useState(() => {
+    return socketClient(SERVER);
+  });
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
-    socket.on('connection', () => {
-      setStatus('Connected');
-      socket.on('message', (message) => {
-        document.getElementById('messages')
-          .appendChild(document.createElement('li'))
-          .innerHTML = JSON.parse(message).message;
+    socket.on("connection", () => {
+      setStatus("Connected");
+      socket.on("message", (message) => {
+        document
+          .getElementById("messages")
+          .appendChild(document.createElement("li")).innerHTML =
+          JSON.parse(message).message;
         messagesEndRef.current?.scrollIntoView();
       });
     });
     return () => {
-      socket.off('connection');
-    }
+      socket.off("connection");
+    };
   }, [socket]);
 
   useEffect(() => {
     if (message === null) return;
-    socket.emit('message', {
-      message: message
+    socket.emit("message", {
+      message: message,
     });
   }, [message]);
 
-  
-
   return (
     <>
-<div className="content">
-
-  <div className="Left">
-    <div className="acc"><h1>{status}</h1></div>
-    <div className="online">online</div>
-    <div className="changelog">change</div>
-  </div>
-    
-
-  <div className="Right">
-        <div className="chat_box">
-          <ul id="messages"></ul>
-          <div ref={messagesEndRef}></div>
+      <div className="content">
+        <div className="Left">
+          <div className="acc">
+            <h1>{status}</h1>
+          </div>
+          <div className="online">online</div>
+          <div className="changelog">change</div>
         </div>
 
-        <div className="input_box">
-          <div className="in">
-            <input type="text" id="message-input" className="inp"></input>
-            <button className="btn" id="sender" onClick={(e) => {
-              e.preventDefault()
-              let messageInput = document.getElementById('message-input');
-              let isEmpty = true;
-              for (let i = 0; i < (messageInput.value).length; i++) {
-                if (messageInput.value[i] != " ") {
-                  isEmpty = false;
-                  break;
-                }
-              }
-              if (messageInput.value === null || messageInput.value === "" || isEmpty) return;
-              setMessage(messageInput.value);
-              messageInput.value = ""
-            }}>Send</button>
+        <div className="Right">
+          <div className="chat_box">
+            <ul id="messages"></ul>
+            <div ref={messagesEndRef}></div>
+          </div>
+
+          <div className="input_box">
+            <div className="in">
+              <input type="text" id="message-input" className="inp"></input>
+              <button
+                className="btn"
+                id="sender"
+                onClick={(e) => {
+                  e.preventDefault();
+                  let messageInput = document.getElementById("message-input");
+                  let isEmpty = true;
+                  for (let i = 0; i < messageInput.value.length; i++) {
+                    if (messageInput.value[i] != " ") {
+                      isEmpty = false;
+                      break;
+                    }
+                  }
+                  if (
+                    messageInput.value === null ||
+                    messageInput.value === "" ||
+                    isEmpty
+                  )
+                    return;
+                  setMessage(messageInput.value);
+                  messageInput.value = "";
+                }}
+              >
+                Send
+              </button>
+            </div>
           </div>
         </div>
-  </div>
-</div>
-
-
+      </div>
     </>
-  )
+  );
 }
