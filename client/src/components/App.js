8058d5cbcecbef0b6004e0d787b81c7ae24a1a82
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2022-03-29 14:20:02 +0200

    design

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,15 +15,28 @@ export default function App() {
     socket.on("connection", () => {
       setStatus("Connected");
       socket.on("message", (message) => {
-        document
-          .getElementById("messages")
-          .appendChild(document.createElement("li")).innerHTML =
-          JSON.parse(message).message;
+        const chat = document.getElementById("messages");
+        let mestext = JSON.parse(message).message;
+        let username = "no-name"
+
+        const mes = `
+        <div class="mes-box">
+          <div class="user-name">
+            ${username}
+          </div>
+
+          <div class="user-text">
+            ${mestext}
+          </div>
+        </div>
+        `;
+        
+        chat.insertAdjacentHTML('afterbegin', mes);
         messagesEndRef.current?.scrollIntoView();
       });
     });
     return () => {
-      socket.off("connection");
+      socket.off("connection"); 
     };
   }, [socket]);
 
@@ -39,15 +52,63 @@ export default function App() {
       <div className="content">
 
         <div className="Left">
-          <div className="acc"><h1>{status}</h1></div>
-          <div className="online">online</div>
-          <div className="changelog">change</div>
+          <div className="acc">
+            <div className="acc-header">
+                <div>ACCOUNT: <h className="green-text" > {status} </h></div>
+            </div>
+
+            <div className="acc-content">
+                <div>NAME:  <h className="green-text" > name</h></div>
+            </div>
+
+            <div className="acc-footer">
+              <button className="log-btn">Log out/in</button>
+            </div>
+            
+          </div>
+
+          <div className="online">
+            <div className="onl-header">
+              <div>ONLINE (N)</div>
+            </div>
+
+            <div className="onl-content">
+              <div className="onl-user">hard.tender.blade</div>
+              <div className="onl-user">hard.tender.blade</div>
+              <div className="onl-user">hard.tender.blade</div>
+              <div className="onl-user">hard.tender.blade</div>
+              <div className="onl-user">hard.tender.blade</div>
+            </div>
+          </div>
+
+          <div className="changelog">
+          <div className="chlog-header">
+              <div>CHANGELOG</div>
+          </div>
+
+          <div className="chlog-content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </div>
+
+          </div>
         </div>
 
 
         <div className="Right">
           <div className="chat_box">
-            <ul id="messages"></ul>
+            <div className="all-mes" id="messages">
+
+              <div class="mes-box">
+                <div class="user-name">
+                    
+                </div>
+
+                <div class="user-text">
+                
+                </div>
+              </div>
+
+            </div>
             <div ref={messagesEndRef}></div>
           </div>
 
@@ -71,44 +132,6 @@ export default function App() {
                 }}>Send</button>
               </div>
             </form>
-          </div>
-
-          <div className="Right">
-            <div className="chat_box">
-              <ul id="messages"></ul>
-              <div ref={messagesEndRef}></div>
-            </div>
-
-            <div className="input_box">
-              <div className="in">
-                <input type="text" id="message-input" className="inp"></input>
-                <button
-                  className="btn"
-                  id="sender"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    let messageInput = document.getElementById("message-input");
-                    let isEmpty = true;
-                    for (let i = 0; i < messageInput.value.length; i++) {
-                      if (messageInput.value[i] != " ") {
-                        isEmpty = false;
-                        break;
-                      }
-                    }
-                    if (
-                      messageInput.value === null ||
-                      messageInput.value === "" ||
-                      isEmpty
-                    )
-                      return;
-                    setMessage(messageInput.value);
-                    messageInput.value = "";
-                  }}
-                >
-                  Send
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
