4cda2990bc2b2eefd9d93ad0cfc699535fc0df22
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2022-05-26 21:55:53 +0200

    responsive for phones +bug

--- client/src/components/bundles/RightComponents/NavBar.jsx
+++ client/src/components/bundles/RightComponents/NavBar.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from 'react-router-dom';
 import mystyle from "../ModuleCss/My.module.css";
 import {CgProfile} from 'react-icons/cg';
+import BL from '../icons/BurgerLines.png';
+import BX from '../icons/BurgerX.png';
+
 
 const NavBar = (props) => {
+    const [isClose, setIsClose] = useState(true);
 
     return (
         <div className={mystyle.HeaderBar}>
-            <div className={mystyle.Name}>
-                <Link to="/Login" className={mystyle.NavBarLink}>{props.name}</Link>
-            </div>
-            <CgProfile style={{height: '2rem', width: '2rem', marginRight: '1rem'}}/>
+
+        <div className={mystyle.WithOutMenu}>
+            <div className={mystyle.Name}><Link to="/Login" className={mystyle.NavBarLink}>{props.name}</Link></div>
+            <div className={mystyle.ManIcon}><CgProfile style={{height: '2rem', width: '2rem'}}/></div>
+            <div className={mystyle.NavBarText}><span className={mystyle.ColorText}>Delta</span>Chat</div>
+
+
+            <button id="Burger" className={mystyle.Burger} onClick={setIsClose(!isClose)}>
+                <div id="BL"><img src={isClose ? BL : BX} alt=""></img></div>
+            </button>
         </div>
+
+        <div id="WithMenu" className={mystyle.WithMenu}>
+                    <div className={mystyle.Menu}>
+
+                        <div className={mystyle.MenuName}>Jmeno</div>
+
+                        <div className={mystyle.MenuBetweenLine}></div>
+
+                        <div className={mystyle.NumOfPeople}><span className={mystyle.BigSizeText}>Právě připojeno 10 lidí</span></div>
+
+                        <div className={mystyle.ListContainer}>
+                            <div className={mystyle.PeopleList}>● Some Jmeno</div>
+                            <div className={mystyle.PeopleList}>● Some Jmeno</div>
+                            <div className={mystyle.PeopleList}>● Some Jmeno</div>
+                            <div className={mystyle.PeopleList}>● Some Jmeno</div>
+                        </div>
+                        
+                    </div> 
+        </div>
+        
+        </div>
+
+
+        
     )
 }
 
