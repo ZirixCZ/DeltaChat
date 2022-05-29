f3d3c0edcb7d46613ad3abdb75cd9c503e249d34
Michal Vaniš <zirixcz@gmail.com>
2022-05-29 23:39:43 +0200

    [Feature] Mobile send button, color scheme

--- client/src/components/bundles/RightComponents/MessageInputBar.jsx
+++ client/src/components/bundles/RightComponents/MessageInputBar.jsx
@@ -6,6 +6,33 @@ import socket from "../../../modules/Socket";
 const MessageInputBar = (props) => {
     const [message, setMessage] = useState(null);
     const [temporaryMessage, setTemporaryMessage] = useState("");
+    const [width, setWindowWidth] = useState(0)
+
+    useEffect(() => {
+        updateDimensions();
+        window.addEventListener("resize", updateDimensions);
+        return () =>
+            window.removeEventListener("resize", updateDimensions);
+        }, [])
+
+    const updateDimensions = () => {
+        const width = window.innerWidth
+        setWindowWidth(width)
+    }
+
+    const btnStyle = {
+        display: (width > 1028) ? 'none' : 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '10rem',
+        backgroundColor: '#262337',
+        borderRight: 'none',
+        borderTop: 'none',
+        borderBottom: 'none',
+        borderLeft: '2px solid black'
+    }
+
+    console.log(btnStyle)
 
     useEffect(() => {
         if (message === null) return;
@@ -17,31 +44,37 @@ const MessageInputBar = (props) => {
 
     return (
         <form className={mystyle.MessageInputBar}>
-            <input className={mystyle.Input} type="text" value={temporaryMessage} placeholder="Pošlete zprávu..."
-                   onChange={(e) => {
-                       setTemporaryMessage(e.target.value);
-                   }}></input>
-            <button style={{display: 'none'}}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        //let messageInput = document.getElementById("message-input");
-                        let isEmpty = true;
-                        if (temporaryMessage === null || temporaryMessage === "") {
-                            return;
-                        }
-                        for (let i = 0; i < temporaryMessage.length; i++) {
-                            if (temporaryMessage[i] !== " ") {
-                                isEmpty = false;
-                                break;
+            <div style={{height: '100%', display: 'flex', flexDirection: 'row'}}>
+                <input className={mystyle.Input} type="text" value={temporaryMessage} placeholder="Pošlete zprávu..."
+                       onChange={(e) => {
+                           setTemporaryMessage(e.target.value);
+                       }}></input>
+                <button style={btnStyle}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            //let messageInput = document.getElementById("message-input");
+                            let isEmpty = true;
+                            if (temporaryMessage === null || temporaryMessage === "") {
+                                return;
                             }
-                        }
-                        if (isEmpty)
-                            return;
-                        setMessage(temporaryMessage);
-                        setTemporaryMessage("");
+                            for (let i = 0; i < temporaryMessage.length; i++) {
+                                if (temporaryMessage[i] !== " ") {
+                                    isEmpty = false;
+                                    break;
+                                }
+                            }
+                            if (isEmpty)
+                                return;
+                            setMessage(temporaryMessage);
+                            setTemporaryMessage("");
 
-                    }}
-            ></button>
+                        }}
+                >
+                    <p style={{color: '#92909B', fontWeight: 'bold'}}>Odeslat</p>
+                </button>
+            </div>
+
+
         </form>
     )
 }
